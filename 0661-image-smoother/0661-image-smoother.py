946c211a0b9dbb4d2ebb11b59da59da2600c8adb
hotel9uest <kk4417jh@gmail.com>
2022-10-19 12:43:34 +0900

    Time: 1735 ms (26.04%), Space: 14.7 MB (34.91%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.py
+++ 0661-image-smoother/0661-image-smoother.py
@@ -1,35 +1,33 @@
 class Solution:
     def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
+        if len(img) <= 0 or len(img[0]) <= 0:
+            return None
+        
+        m = len(img)
+        n = len(img[0])
 
-        def averageCell(img: List[List[int]], x: int, y:int) -> int:
-            if len(img) <= 0 or len(img[0]) <= 0:
-                return 0
-
+        def averageCell(x: int, y:int) -> int:
             count = 0
             summary = 0
 
-            for i in range(x-1, x+2):
-                if i < 0 or i >= len(img[0]):
+            for j in range(y-1, y+2):
+                if j < 0 or j >= m:
                     continue
 
-                for j in range(y-1, y+2):
-                    if j < 0 or j >= len(img):
+                for i in range(x-1, x+2):
+                    if i < 0 or i >= n:
                         continue
 
                     count = count + 1
                     summary = summary + img[j][i]
 
-            return math.floor(summary/count) if count > 0 and summary > 0 else 0
+            return summary // count if count > 0 and summary > 0 else 0
 
-        if len(img) <= 0 or len(img[0]) <= 0:
-            return None
-        
         result = []
 
-        for j in range(0, len(img)):
+        for j in range(0, m):
             result.append([])
-            for i in range(0, len(img[j])):
-                result[j].append(averageCell(img, i, j))
+            for i in range(0, n):
+                result[j].append(averageCell(i, j))
         
         return result
-
