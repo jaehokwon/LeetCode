0fc5b0d35cd95537edbbb94b4b798a90495b39db
hotel9uest <kk4417jh@gmail.com>
2022-11-01 16:59:28 +0900

    Time: 68 ms (24.51%), Space: 13.8 MB (98.04%) - LeetHub

--- 0925-long-pressed-name/0925-long-pressed-name.py
+++ 0925-long-pressed-name/0925-long-pressed-name.py
@@ -1,27 +1,12 @@
 class Solution:
     def isLongPressedName(self, name: str, typed: str) -> bool:
-        dic_n, dic_t = [], []
+        i, j, n, t = 0, 0, len(name), len(typed)
 
-        for n in name:
-            if len(dic_n) > 0 and n in dic_n[-1]:
-                dic_n[-1][n] += 1
-            else:
-                dic_n.append({n: 1})
-        
-        for t in typed:
-            if len(dic_t) > 0 and t in dic_t[-1]:
-                dic_t[-1][t] += 1
-            else:
-                dic_t.append({t: 1})
-        
-        if len(dic_n) != len(dic_t):
-            return False
-        
-        for i in range(len(dic_n)):
-            key_n = list(dic_n[i].keys())[0]
-            key_t = list(dic_t[i].keys())[0]
-
-            if key_n != key_t or dic_n[i][key_n] > dic_t[i][key_t]:
+        while j < t:
+            if i < n and name[i] == typed[j]:
+                i += 1
+            elif j == 0 or name[i - 1] != typed[j]:
                 return False
-        
-        return True
+            j += 1
+
+        return i == n
