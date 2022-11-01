b874c5ba1ec29402112c29298fa48a3e0921f99b
hotel9uest <kk4417jh@gmail.com>
2022-11-01 15:53:16 +0900

    Time: 87 ms (11.06%), Space: 14.1 MB (77.19%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,17 +1,16 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        a = []
-        self.dp(n, 1, 0, '(', a)
-        return a
+        result = []
+        self.dfs(n, 1, 0, '(', result)
+        return result
     
-    def dp(self, n, i, j, cur_str, lst):
+    def dfs(self, n, i, j, cur_str, lst):
         if i < j or i > n or j > n:
             return
 
         if n == i == j:
-            if cur_str not in lst:
-                lst.append(cur_str)
+            lst.append(cur_str)
             return
         
-        self.dp(n, i+1, j, cur_str + '(', lst)
-        self.dp(n, i, j+1, cur_str + ')', lst)
+        self.dfs(n, i+1, j, cur_str + '(', lst)
+        self.dfs(n, i, j+1, cur_str + ')', lst)
