78b5a83897da4608c909db5dbb53d6fedec60a73
hotel9uest <kk4417jh@gmail.com>
2022-11-10 14:53:01 +0900

    Time: 1656 ms (37.79%), Space: 18 MB (29.03%) - LeetHub

--- 2241-design-an-atm-machine/2241-design-an-atm-machine.py
+++ 2241-design-an-atm-machine/2241-design-an-atm-machine.py
@@ -1,49 +1,21 @@
 class ATM:
 
     def __init__(self):
-        self.dollors = [0, 0, 0, 0, 0]
+        self.cash = [0] * 5
+        self.values = [20, 50, 100, 200, 500]
 
     def deposit(self, banknotesCount: List[int]) -> None:
-        for i in range(len(banknotesCount)):
-            self.dollors[i] += banknotesCount[i]
-        
-        print(self.dollors)
+        for i, val in enumerate(banknotesCount):
+            self.cash[i] += val
 
     def withdraw(self, amount: int) -> List[int]:
-        res = [0, 0, 0, 0, 0]
-
-        for i in range(len(self.dollors) - 1, -1, -1):
-            if amount > 0:
-                cnt, tot = self.availableWithdraw(amount, i)
-                if cnt > 0:
-                    res[i] = cnt
-                    amount -= tot
-        
+        res = []
+        for n, val in zip(self.cash[::-1], self.values[::-1]):
+            need = min(n, amount // val)
+            res = [need] + res
+            amount -= need * val
         if amount == 0:
-            for i in range(len(self.dollors)):
-                self.dollors[i] -= res[i]
-
-        print(res if amount == 0 else [-1])
-
-        return res if amount == 0 else [-1]
-    
-    def availableWithdraw(self, amount: int, denominations: int) -> Tuple[int, int]:
-        convert_dollor = 0
-
-        if denominations == 0:
-            convert_dollor = 20
-        elif denominations == 1:
-            convert_dollor = 50
-        elif denominations == 2:
-            convert_dollor = 100
-        elif denominations == 3:
-            convert_dollor = 200
+            self.deposit([-x for x in res])
+            return res
         else:
-            convert_dollor = 500
-        
-        cnt = amount // convert_dollor
-
-        if self.dollors[denominations] < cnt:
-            cnt = self.dollors[denominations]
-        
-        return cnt, cnt * convert_dollor
+            return [-1]
